64b3d4c9b754ed1dd21f453b5cbddaacc1b9973f
Yulya <ylhappy999@gmail.com>
2024-10-21 00:07:33 +1000

    3.5 Import [243567]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,23 @@
+package ru.job4j.math;
+
+
+public static class mathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+}
+public static double multiply(double first, double second) {
+    return first * second;
+}
+
+public static double subtract(double first, double second) {
+    return first - second;}
+public static double divide(double first, double second) {
+    return first / second;
+}
+
+public static void main(String[] args) {
+    System.out.println("Результат расчета равен: " + mathFunction);
+
+}
+
