65c4b0e72be71811be30a7243f7b2891b11d6e13
Ylhappy005 <mydream9995@gmail.com>
2024-10-22 04:51:20 +1000

    Update Vector.java

--- src/main/java/ru/job4j/condition/Vector.java
+++ src/main/java/ru/job4j/condition/Vector.java
@@ -1,4 +1,6 @@
 package ru.job4j.condition;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Vector {
     public static String addVectors(int x1, int y1, int x2, int y2) {
@@ -10,5 +12,31 @@ public class Vector {
     public static void main(String[] args) {
         String result = addVectors(1, 2, 3, 4);
         System.out.println("result (1, 2) + (3, 4) = " + result);
+    }
+}
+
+class VectorTest {
+    @Test
+    void whenVectors12And34ThenResult45() {
+        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        String expected = "(4, 6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectorsMinus12And34ThenResult22() {
+        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        String expected = "(2, 2)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenVectorsMinus12AndMinus34ThenResultMinus46() {
+        int x1 = -1, y1 = -2, x2 = -3, y2 = -4;
+        String expected = "(-4, -6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
     }
 }
